bfac5e75531865400f1702b718f03a105be541bf
binit92 <kumar.binit1992@gmail.com>
2017-09-18 12:05:15 +0530

    Task 1 : Project Setup

---
+++ app/src/main/java/com/waxtree/waxtree/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.waxtree.waxtree.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.waxtree.waxtree.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
